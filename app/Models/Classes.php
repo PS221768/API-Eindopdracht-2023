12a0c70f1ee10a9d5e59deadc7ea6401efaf637a
PS221768 <PS221768@edu.summacollege.nl>
2023-04-17 12:11:35 +0200

    Added / Changed Models
    (untested) added models, these will be tested, and changed if wrong later when we have the api controllers up and running

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Classes extends Model
 {
     use HasFactory;
+
+    public function students()
+    {
+        return $this->belongsToMany(Students::class);
+    }
 }
